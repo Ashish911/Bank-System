478392969df969dc2de83a303867e66d30a45921
Ashish911 <ashishdongol32@gmail.com>
2025-06-13 20:30:29 +0100

    API Documentation Implemented

--- accounts/src/main/java/com/ashish/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/ashish/accounts/AccountsApplication.java
@@ -1,11 +1,42 @@
 package com.ashish.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+//@ComponentScan("com.ashish.accounts.controller")
+//@EnableJpaRepositories("com.ashish.accounts.repository")
+//@EntityScan("com.ashish.accounts.entity")
 @EnableJpaAuditing(auditorAwareRef = "AuditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Account microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "1.0",
+				contact = @Contact(
+						name = "Ashish Dongol",
+						email = "ashishDongol32@gmail.com",
+						url = "https://github.com/ashishDongol"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.apache.org/licenses/LICENSE-2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Accounts microservice REST API Documentation",
+				url = "https://github.com/ashishDongol"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
