fbe6b47b74a20d598fe4a9e89cccd153f6571a39
Ashish911 <ashishdongol32@gmail.com>
2025-09-05 23:56:58 +0100

    Implemented Kubernetes Discovery Server

--- accounts/src/main/java/com/ashish/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/ashish/accounts/AccountsApplication.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 //@ComponentScan("com.ashish.accounts.controller")
 //@EnableJpaRepositories("com.ashish.accounts.repository")
 //@EntityScan("com.ashish.accounts.entity")
